8b4fb9b179ee4b91afa04cc4e4c6e2f65a01c128
Daniel Brown <94daniel.brown@gmail.com>
2023-10-04 22:13:07 +0100

    .

--- reverse_string/go.mod
+++ reverse_string/go.mod
@@ -1,3 +1,5 @@
 module reverse_string
 
 go 1.19
+
+require github.com/94DanielBrown/helpers v1.0.0
--- reverse_string/main.go
+++ reverse_string/main.go
@@ -1,8 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"github.com/94DanielBrown/helpers"
+)
+
 func main() {
+	fmt.Println(reverse("abc"))
 }
 
-func reverse(s string) (result string, e error) {
-	return "", nil
+func reverse(s string) (result string) {
+	return helpers.Reverse(s)
+	//	return "", nil
 }
