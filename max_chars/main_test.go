09c476961cc61853a99e9f52f8e2028e6a80ad5b
Daniel Brown <94daniel.brown@gmail.com>
2023-10-09 21:54:37 +0100

    added to reverse string

---
+++ max_chars/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_functionExists(t *testing.T) {
+	if reflect.ValueOf(maxChars).IsNil() {
+		t.Errorf("Function doesn't exist")
+	}
+}
+
+func Test_findsMaxChar(t *testing.T) {
+	maxCharTests := []struct {
+		input    string
+		expected string
+	}{
+		{"abccccccccd", "c"},
+		{"apple 1231111", "1"},
+	}
+
+	for _, e := range maxCharTests {
+		result := maxChars(e.input)
+		if result != e.expected {
+			t.Errorf("Got %n but got %n", result, e.expected)
+		}
+	}
+}
--- reverse_string/main.go
+++ reverse_string/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 )
 
 func main() {
 	fmt.Println(reverse("abc"))
+	fmt.Println(reverse2("abc"))
 }
 
 func reverse(s string) (result string) {
@@ -15,6 +17,12 @@ func reverse(s string) (result string) {
 	for _, char := range slice {
 		reversed = char + reversed
 	}
-	fmt.Println(reversed)
 	return reversed
+}
+
+func reverse2(s string) (result string) {
+	slice := strings.Split(s, "")
+	slices.Reverse(slice)
+	result = strings.Join(slice, "")
+	return result
 }
