6cfd111001cd0cf777643777ad9a4f40165d49ee
Daniel Brown <94daniel.brown@gmail.com>
2023-10-12 09:07:54 +0100

    added anagrams

---
+++ anagrams/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+}
+
+func anagrams(s string, s2 string) (result bool) {
+
+}
---
+++ anagrams/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_functionExists(t *testing.T) {
+	if reflect.ValueOf(anagrams).IsNil() {
+		t.Errorf("Function doesn't exist")
+	}
+}
+
+func Test_chunksArray(t *testing.T) {
+	anagramTests := []struct {
+		firstString  string
+		secondString string
+		expected     bool
+	}{
+		{"", "", true},
+		{"", "", true},
+	}
+
+	for _, e := range anagramTests {
+		result := anagrams(e.firstString, e.secondString)
+		if result != e.expected {
+			t.Errorf("Expected %v but got %v", e.expected, result)
+		}
+	}
+}
